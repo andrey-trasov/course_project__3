4b796d2dcd35d92f7a84f95dcccff28ea20816e7
andrey-trasov <159887936+andrey-trasov@users.noreply.github.com>
2024-02-27 12:57:36 +0300

    the project is done

--- utils/functions.py
+++ utils/functions.py
@@ -1,23 +1,22 @@
 import json
 
-def uploading_file():
+def uploading_file(file_transaction):
     """достаем файлы из json"""
-    with open('operations.json', 'r', encoding='utf-8') as file:
+    with open(file_transaction, 'r', encoding='utf-8') as file:
         raw_json = json.load(file)
         return raw_json
 
 
-def sorting_operations():
+def sorting_operations(raw_json):
     """сортируем дынные: по 'CANCELED' и дате"""
     sort_state = []
-    raw_json = uploading_file()
     for transaction in raw_json:
         if 'state' in transaction:
             if transaction['state'] == 'EXECUTED':
                 sort_state.append(transaction)
 
     sort_state.sort(reverse = True, key=lambda x: x['date'])
-    return sort_state
+    return sort_state[:5]
 
 
 def transaction_output(date, description):
@@ -46,7 +45,3 @@ def quantity(operation):
     передаем сумму и валюту
     """
     return f"{operation["amount"]} {operation["currency"]["name"]}"
-
-
-
-
